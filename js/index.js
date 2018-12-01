22e6c2905ad2eb57cc813eb33e2c84dbc12a8729
Chelsea Allred <chelsdallred@gmail.com>
2018-11-30 17:25:00 -0700

    functioning

--- js/index.js
+++ js/index.js
@@ -1,42 +1,70 @@
-//Computer picks an option
-var rpc = ["Paper", "Rock", "Scissors"]
 
-var computerChoice = rpc[Math.floor(Math.random()*rpc.length)];
+    //User picks an option
+    var userChoice;
+    var rpc = ["Paper", "Rock", "Scissors"];
+    var computerResult;
+    var result;
+    
+    var rock = document.body.querySelector(".rock");
+    rock.addEventListener("click", function(){
+        userChoice = "Rock";
+        computerChoice();
+    });
 
-//User picks an option
-var userChoice;
+    var paper = document.body.querySelector(".paper");
+    paper.addEventListener("click", function(){
+        userChoice = "Paper";
+        computerChoice();
+    });
+    
+    var scissors = document.body.querySelector(".scissors");
+    scissors.addEventListener("click", function(){
+        userChoice = "Scissors";
+        computerChoice();
+    });
 
-var rock = document.body.querySelector(".rock");
-rock.addEventListener("click", function() {
-    userChoice = "Rock";
-    alert('rock')
-})
+    //Computer picks an option
+    function computerChoice(){
+    computerResult = rpc[Math.floor(Math.random()*rpc.length)];
+    console.log(computerResult, userChoice);
+    compare();
+    };
 
-var paper = document.body.querySelector(".paper");
-paper.addEventListener("click", function() {
-    userChoice = "Paper";
-})
+    //Add User & Computer options together and compare
 
-var scissors = document.body.querySelector(".scissors");
-scissors.addEventListener("click", function() {
-    userChoice = "Scissors"
-})
+    function compare(){
+    if(computerResult===userChoice){
+        return "It's a tie!";
+    }
+    if(computerResult==="rock"){
+        if(userChoice==="scissors"){
+            return "rock wins";
+        }
+        else{
+            return "paper wins";
+        }
+    }
+    if(computerResult==="paper"){
+        if(userChoice==="rock"){
+            return "paper wins";
+        }
+        else{
+            return "scissors wins";
+        }
+    }
+    if(computerResult==="scissors"){
+        if(userChoice==="rock"){
+            return "rock wins";
+        }
+        else{
+            return "scissors wins";
+        }
+    }
+    displayResult()
+    };
 
-
-//Add User & Computer options together and compare
-
-switch (userChoice) {
-    case (computerChoice):
-        day = "Sunday";
-        break;
-    case 1:
-        day = "Monday";
-        break;
-    case 2:
-        day = "Tuesday";
-        break;
-    case 3:
-        day = "Wednesday";
-        break;
-}
-//Display user & computer options with winner 
+//Display user & computer options to winner 
+function displayResult(){
+    var resultContainer = document.body.querySelector("#result");
+    resultContainer.innerHTML = result;
+    };
